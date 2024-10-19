b8b0d4fc72685425113af413d76649a2373b1b44
CJ <1143428471@qq.com>
2024-10-19 18:18:41 +0800

    提交代码与测试类

--- Solution12.java
+++ Solution12.java
@@ -17,7 +17,7 @@ import java.util.*;
 
 class Solution {
     public String multiply(String num1, String num2) {
-        if (num1.equals("0") | num2.equals("0")) {
+        if (num1.equals("0") || num2.equals("0")) {
             return "0";
         }
         String ans = "0";
